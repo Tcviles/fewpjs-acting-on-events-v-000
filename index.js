14f01bcef408dcc54d3c48f7e5e03b0520daf7ae
Thomas <Thomas.c.viles.jr@gmail.com>
2021-06-01 09:29:58 -0400

    commit tree 868697539157f421ea69411fdafaca22c64c9ee7

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function moveDodgerLeft() {
+    let dodger = document.getElementById("dodger");
+
+    let leftNumber = parseInt(dodger.style.left.replace("px",""));
+    dodger.style.left = `${leftNumber-10}px`;
+}
+
+function moveDodgerRight() {
+    let dodger = document.getElementById("dodger");
+
+    let leftNumber = parseInt(dodger.style.left.replace("px",""));
+    dodger.style.left = `${leftNumber+10}px`;
+}
